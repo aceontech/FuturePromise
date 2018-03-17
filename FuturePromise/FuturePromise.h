7a973da8d9ecaef0725f6b81d75f3e3817af65e4
Alex Manarpies <alex@jarroo.com>
2018-03-18 00:37:38 +0100

    Initial Commit

---
+++ FuturePromise/FuturePromise.h
@@ -0,0 +1,19 @@
+//
+//  FuturePromise.h
+//  FuturePromise
+//
+//  Created by Alex on 18/03/2018.
+//  Copyright © 2018 Jarroo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FuturePromise.
+FOUNDATION_EXPORT double FuturePromiseVersionNumber;
+
+//! Project version string for FuturePromise.
+FOUNDATION_EXPORT const unsigned char FuturePromiseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FuturePromise/PublicHeader.h>
+
+
